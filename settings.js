fb655268bd14a7aab98a2a30a722b3ad8bb33280
zallmaker <scytzall01@gmail.com>
2022-09-22 08:20:11 +0700

    Hinata2

---
+++ settings.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Free Apikey
+global.APIs = {
+	riy: 'https://api-xriy04.herokuapp.com',
+}
+
+// Other
+global.owner = ['62895330006855']
+global.premium = ['62895330006855']
+global.youtube = 'https://youtube.com/c/RisalTmy'
+global.ownername = '🔥 THE ZALL BOT 🔥'
+global.botname ='Hinata-Md'
+global.donasi = {
+    saweria: 'https://saweria.co/zall87',
+    nomor: '0895330006855'
+}
+global.packname = 'Hinata-Md'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...'
+}
+global.thumb = fs.readFileSync('./image/hinata.jpg')
+global.vn = './sound/menu.mp3'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
